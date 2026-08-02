03224eef9aba8d142d16e243c5129a366ce1c473
froyln <froylanpd74@gmail.com>
2026-08-02 15:27:26 -0700

    Add drop-behind tweak, bump to 1.0.1

--- src/main/java/dev/froyln/dropitems/Reference.java
+++ src/main/java/dev/froyln/dropitems/Reference.java
@@ -4,5 +4,5 @@ public class Reference
 {
     public static final String MOD_ID = "dropdummyitems";
     public static final String MOD_NAME = "DropDummyItems";
-    public static final String MOD_VERSION = "1.0.0";
+    public static final String MOD_VERSION = "1.0.1";
 }
